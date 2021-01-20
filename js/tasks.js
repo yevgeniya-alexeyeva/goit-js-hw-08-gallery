ce7f32d139775426346168b42ca13f6cd6ad3857
Yevgeniya Alexeyeva <y.alekseyeva@gmail.com>
2021-01-20 12:54:29 +0600

    6th task fixed

--- js/tasks.js
+++ js/tasks.js
@@ -47,7 +47,7 @@ console.log(getUserWithEmail(users, "elmahead@omatom.com")); // {объект п
 // Задача 6
 
 const getUsersWithAge = (users, min, max) => {
-  return users.filter((user) => user.age > min && user.age < max);
+  return users.filter((user) => user.age >= min && user.age <= max);
 };
 
 console.log(getUsersWithAge(users, 20, 30)); // [объект Ross Vazquez, объект Elma Head, объект Carey Barr]
